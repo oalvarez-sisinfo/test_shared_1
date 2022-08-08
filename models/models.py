816403e093c78df73523716c32c83e3dce2067a5
oalvarez-sisinfo <110239019+oalvarez-sisinfo@users.noreply.github.com>
2022-08-08 11:03:20 -0300

    clear

--- models/models.py
+++ models/models.py
@@ -1,17 +1,3 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
-
-# Line from 1
-# Line from 2
-# class test_module(models.Model):
-#     _name = 'test_module.test_module'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
